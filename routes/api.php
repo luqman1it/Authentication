8a565293afc7d139f7353e992179caafe12fe70a
luqman1it <luqman1it@gmail.com>
2024-03-19 14:42:13 +0300

    edit route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\LoginRegisterController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(LoginRegisterController::class)->group(function () {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
 });
